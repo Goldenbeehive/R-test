9c7659f2726f4be90963a448495b8bc56a4ab857
Golden <46891447+Goldenbeehive@users.noreply.github.com>
2026-09-24 22:31:45 +0300

    final app.js removed hello world example

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,13 +1,16 @@
 const express = require('express')
-
- 
+const authRoutes = require('./routes/authRoutes')
+const projectRoutes = require('./routes/projectRoutes')
+const taskRoutes = require('./routes/taskRoutes')
+const errorMiddleware = require('./middleware/errorMiddleware')
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
- 
+
+app.use('/auth', authRoutes)
+app.use('/projects', projectRoutes)
+app.use('/tasks', taskRoutes)
+app.use(errorMiddleware)
 
 module.exports = app
